840dbbeffae4c553e1c1febde06ad047f323767e
cree321 <42984414+cree321@users.noreply.github.com>
2020-11-22 19:23:40 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,31 +1,14 @@
 "use strict";
-/*\\ Asynchronous Code \\*/
-var renderDelegate = new Worker("js/renderDelegate.js");
-
-// Pointer
-/*window.addEventListener("pointerdown", (e) => renderDelegate.postMessage({e}));
-window.addEventListener("pointerup", (e) => renderDelegate.postMessage({e}));*/
-window.addEventListener("mousemove", (e) => renderDelegate.postMessage(Object.assign({}, e)));
-// Keyboard
-window.addEventListener("keydown", (e) => renderDelegate.postMessage(Object.assign({}, e)));
-window.addEventListener("keyup", (e) => renderDelegate.postMessage(Object.assign({}, e)));
-// Worker
+const renderDelegate = new Worker("js/renderDelegate.js");
 renderDelegate.onmessage = (e) => {escena.style.transform = e.data;}
-//
-/*\\ Synchronous Code \\*/
-const escena = document.getElementById("escena");
 
-
-
-
-
-function loop() {
-  
-}
-/*rotation.forEach(value, i => value += deltaRotation[i]);
-  deltaRotation = [0,0];*/
 
 window.onload = (event) => {
-  //const escena = document.getElementById("escena");
+  const escena = document.getElementById("escena");
+  window.addEventListener("keydown", (e) => renderDelegate.postMessage(Object.assign({}, e)));
+  window.addEventListener("keyup", (e) => renderDelegate.postMessage(Object.assign({}, e)));
+  //window.addEventListener("pointerdown", (e) => renderDelegate.postMessage({e}));
+  //window.addEventListener("pointerup", (e) => renderDelegate.postMessage({e}));
+  //window.addEventListener("mousemove", (e) => renderDelegate.postMessage(Object.assign({}, e)));
   //var gameLoop = setInterval(loop, 50);
 }
