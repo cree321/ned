33cf838bbe456e574ccf4a2897207796514cbd86
cree321 <42984414+cree321@users.noreply.github.com>
2020-11-22 23:48:04 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,9 +3,17 @@ const renderDelegate = new Worker("js/renderDelegate.js");
 renderDelegate.onmessage = (e) => {escena.style.transform = e.data;}
 
 
+function replacer(key, value) {
+  // Filtering out properties
+  if (typeof value === 'string') {
+    return undefined;
+  }
+  return value;
+}
+
 window.onload = (event) => {
   const escena = document.getElementById("escena");
-  window.addEventListener("keydown", (e) => renderDelegate.postMessage(JSON.stringify(e, ["type", "data"])));
+  window.addEventListener("keydown", (e) => renderDelegate.postMessage(JSON.stringify(e, ['type', 'data'])));
   window.addEventListener("keyup", (e) => renderDelegate.postMessage(Object.assign({}, e)));
   //window.addEventListener("pointerdown", (e) => renderDelegate.postMessage({e}));
   //window.addEventListener("pointerup", (e) => renderDelegate.postMessage({e}));
