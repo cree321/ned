d80bdfe2d39bd9ecddfc73b086a9a0b805b14e0f
cree321 <42984414+cree321@users.noreply.github.com>
2020-11-24 15:04:03 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ renderDelegate.onmessage = (e) => {escena.style.transform = e.data;}
 
 window.onload = (event) => {
   const escena = document.getElementById("escena");
-  window.addEventListener("keydown", (e) => renderDelegate.postMessage({type: e.typeArg, data: e.KeyboardEventInit}));
+  window.addEventListener("keydown", (e) => renderDelegate.postMessage(new KeyboardEvent(e.typeArg, e.KeyboardEventInit)));
   window.addEventListener("keyup", (e) => renderDelegate.postMessage(Object.assign({}, e)));
   //window.addEventListener("pointerdown", (e) => renderDelegate.postMessage({e}));
   //window.addEventListener("pointerup", (e) => renderDelegate.postMessage({e}));
