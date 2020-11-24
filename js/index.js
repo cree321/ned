c1ce61869d3eedc27b71bfb3ae3c540a2712f9c6
cree321 <42984414+cree321@users.noreply.github.com>
2020-11-24 15:41:39 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ renderDelegate.onmessage = (e) => {escena.style.transform = e.data;}
 
 window.onload = (event) => {
   const escena = document.getElementById("escena");
-  window.addEventListener("keydown", (e) => {type: 1, eventData: e.key});/*
+  window.addEventListener("keydown", (e) => renderDelegate.postMessage({type: 1, eventData: e.key}));/*
     var event = new e.constructor(e.type, e);
     renderDelegate.postMessage(event);
   });*/
