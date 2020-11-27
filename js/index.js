4420cace39b340fd6aa2b5ae41177d512a0e4f7c
cree321 <42984414+cree321@users.noreply.github.com>
2020-11-26 23:05:13 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,11 +6,12 @@ renderDelegate.onmessage = (e) => {
   else {escena.style.transform = e.data.transform;}
 }
 const sceneDelegate = new Worker("js/sceneDelegate.js");
-sceneDelegate.onmessage = (e) => {escena.innerHTML = e.data.scene;}
+sceneDelegate.onmessage = (e) => {escena.innerHTML = e.data;}
 
 window.onload = (event) => {
   const camera = document.getElementById("camera");
   const escena = document.getElementById("escena");
+  sceneDelegate.postMessage();
   window.addEventListener("keydown", (e) => {if(!e.repeat) renderDelegate.postMessage({type: 1, code: e.code});});
   window.addEventListener("keyup", (e) => renderDelegate.postMessage({type: 0, code: e.code}));
   //window.addEventListener("pointerdown", (e) => renderDelegate.postMessage({e}));
