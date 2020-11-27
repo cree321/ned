bf7884acf851856f7e41f1b625773f169ec895dc
cree321 <42984414+cree321@users.noreply.github.com>
2020-11-26 23:37:54 -0800

    Update sceneDelegate.js

--- js/sceneDelegate.js
+++ js/sceneDelegate.js
@@ -1,11 +1,25 @@
 "use strict";
-var cap0 = JSON.parse("assets/cap0.json");
+var commit = ``;
+const cap0ul = new Request("assets/cap0.json");
+
+parseMap(cap0ul);
 
 onmessage = () => {
-  var commit = ``;
-  cap0.hall.forEach((value) => {
+  /*cap0.hall.forEach((value) => {
     commit += `<div class="geo" style="transform: translate3d(${value.t[0]}px,${value.t[1]}px,${value.t[2]}px) scale3d(${value.s[0]},${value.s[1]},${value.s[2]}) rotateX(${value.r[0]}) rotateY(${value.r[1]})">
 </div>`;
-  });
+  });*/
   postMessage(commit);
+}
+
+function parseMap(request) {
+  commit = ``;
+  fetch(request)
+    .then(response => response.json())
+    .then(data => {
+      data.hall.forEach((value) => {
+        commit += `<div class="geo" style="transform: translate3d(${value.t[0]}px,${value.t[1]}px,${value.t[2]}px) scale3d(${value.s[0]},${value.s[1]},${value.s[2]}) rotateX(${value.r[0]}) rotateY(${value.r[1]})">
+</div>`;
+  });
+    }).catch(console.error);
 }
