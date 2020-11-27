7cf297c782a52e61d7cdec63a96bc33976ceeaf3
cree321 <42984414+cree321@users.noreply.github.com>
2020-11-26 23:38:52 -0800

    Update sceneDelegate.js

--- js/sceneDelegate.js
+++ js/sceneDelegate.js
@@ -4,7 +4,7 @@ const cap0ul = new Request("assets/cap0.json");
 
 parseMap(cap0ul);
 
-onmessage = () => {
+onmessage = (message) => {
   /*cap0.hall.forEach((value) => {
     commit += `<div class="geo" style="transform: translate3d(${value.t[0]}px,${value.t[1]}px,${value.t[2]}px) scale3d(${value.s[0]},${value.s[1]},${value.s[2]}) rotateX(${value.r[0]}) rotateY(${value.r[1]})">
 </div>`;
