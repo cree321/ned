1f63e91c9210764a1273ed8f87243ed5b321ed00
cree321 <42984414+cree321@users.noreply.github.com>
2020-11-26 22:57:04 -0800

    Update sceneDelegate.js

--- js/sceneDelegate.js
+++ js/sceneDelegate.js
@@ -1,1 +1,11 @@
-onmessage = (message) => {}
+"use strict";
+var cap0 = JSON.parse("assets/cap0.json");
+
+onmessage = (message) => {
+  var commit = ``;
+  cap0.hall.forEach((value) => {
+    commit += `<div class="geo" style="transform: translate3d(${value.t[0]}px,${value.t[1]}px,${value.t[2]}px) scale3d(${value.s[0]},${value.s[1]},${value.s[2]}) rotateX(${value.r[0]}) rotateY(${value.r[1]})">
+</div>`;
+  });
+  postMessage(commit);
+}
