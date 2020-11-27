d7e2c25529feaaebfbbf69e40b2a3a91dc96989d
cree321 <42984414+cree321@users.noreply.github.com>
2020-11-26 21:52:06 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ renderDelegate.onmessage = (e) => {
   if(e.data.type) {camera.style.transform = "translateZ(800px) "+e.data.transform;}
   else {escena.style.transform = e.data.transform;}
 }
-/*const sceneDelegate = new Worker("js/sceneDelegate.js");
-sceneDelegate.onmessage = (e) => {console.log(e);}*/
-
+const sceneDelegate = new Worker("js/sceneDelegate.js");
+sceneDelegate.onmessage = (e) => {escena.innerHTML = e.data.scene;}
 
 window.onload = (event) => {
   const camera = document.getElementById("camera");
   const escena = document.getElementById("escena");
+  console.log(escena.innerHTML);
   window.addEventListener("keydown", (e) => {if(!e.repeat) renderDelegate.postMessage({type: 1, code: e.code});});
   window.addEventListener("keyup", (e) => renderDelegate.postMessage({type: 0, code: e.code}));
   //window.addEventListener("pointerdown", (e) => renderDelegate.postMessage({e}));
@@ -19,3 +19,4 @@ window.onload = (event) => {
   //window.addEventListener("mousemove", (e) => renderDelegate.postMessage(Object.assign({}, e)));
   //var gameLoop = setInterval(loop, 50);
 }
+
