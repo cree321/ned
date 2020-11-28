74f2f2492907e944b2ae2d6412eac519e5250d9e
cree321 <42984414+cree321@users.noreply.github.com>
2020-11-27 21:39:01 -0800

    Update sceneDelegate.js

--- js/sceneDelegate.js
+++ js/sceneDelegate.js
@@ -1,5 +1,5 @@
 "use strict";
-var commit = ``;
+var commit = '';
 const cap0ul = new Request("https://raw.githubusercontent.com/cree321/cree321.github.io/master/assets/cap0.json");
 
 parseMap(cap0ul);
@@ -13,13 +13,13 @@ onmessage = (message) => {
 }
 
 function parseMap(request) {
-  commit = ``;
+  commit = '';
   fetch(request)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-      /*data.hall.forEach((value) => {
+      //console.log(data);
+      data.hall.forEach((value) => {
         commit += `<div class="geo" style="transform: translate3d(${value.t[0]}px,${value.t[1]}px,${value.t[2]}px) scale3d(${value.s[0]},${value.s[1]},${value.s[2]}) rotateX(${value.r[0]}deg) rotateY(${value.r[1]}deg);"></div>`;
-      });*/
+      });
     }).catch(console.error);
 }
