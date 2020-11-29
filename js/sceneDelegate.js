b54b542b09a0ed0f040007e0143e65a77860f502
cree321 <42984414+cree321@users.noreply.github.com>
2020-11-28 16:38:19 -0800

    Update sceneDelegate.js

--- js/sceneDelegate.js
+++ js/sceneDelegate.js
@@ -25,8 +25,9 @@ function parseMap(request) {
     .then(response => response.json())
     .then(data => {
       //console.log(data);
+      commit += `<link rel="stylesheet" href="${data.link}"></link>`;
       data.hall.forEach((value) => {
-        commit += `<div class="geo" style="transform: translate3d(${value.t[0]}px,${value.t[1]}px,${value.t[2]}px) scale3d(${value.s[0]},${value.s[1]},${value.s[2]}) rotateX(${value.r[0]}deg) rotateY(${value.r[1]}deg);"></div>`;
+        commit += `<div class="geo ${value.c}" style="transform: translate3d(${value.t[0]}px,${value.t[1]}px,${value.t[2]}px) scale3d(${value.s[0]},${value.s[1]},${value.s[2]}) rotateX(${value.r[0]}deg) rotateY(${value.r[1]}deg);"></div>`;
       });
     }).catch(console.error);
 }
