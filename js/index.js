9010c1f9b4c5d3c34562352a4ab002e345df9a55
cree321 <42984414+cree321@users.noreply.github.com>
2020-11-28 20:25:38 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,14 +10,18 @@ sceneDelegate.onmessage = (e) => {escena.innerHTML = e.data;}
 
 window.onload = (event) => {
   const viewport = document.getElementById("viewport");
+  const camera = document.getElementById("camera");
+  const escena = document.getElementById("escena");
   
   viewport.addEventListener("click", (e) => {
-    const camera = document.getElementById("camera");
-    const escena = document.getElementById("escena");
     sceneDelegate.postMessage(null);
+    viewport.style.animation-name = "viewport";
+    viewport.style.animation-duration: "3s";
+    viewport.style.animation-fill-mode: "both";
+    setTimeout(() => {viewport.style.animation = none;}, 3010);
     window.addEventListener("keydown", (e) => {if(!e.repeat) renderDelegate.postMessage({type: 1, code: e.code});});
     window.addEventListener("keyup", (e) => renderDelegate.postMessage({type: 0, code: e.code}));
-  });
+  }, { once: true });
   //window.addEventListener("pointerdown", (e) => renderDelegate.postMessage({e}));
   //window.addEventListener("pointerup", (e) => renderDelegate.postMessage({e}));
   //window.addEventListener("mousemove", (e) => renderDelegate.postMessage(Object.assign({}, e)));
